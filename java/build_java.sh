a7c23c66e5cef4056055070fc58102aa27825824
Truong Hoang Phuong <thp.1093@outlook.com>
2019-08-07 12:31:31 +0700

    fix java build for Java version >= 10

--- java/build_java.sh
+++ java/build_java.sh
@@ -12,8 +12,7 @@ JAVA_COMPILER=`readlink -f \`which javac\``
 JAVA_HOME=${JAVA_COMPILER%%/bin/javac}
 
 mkdir -p ${BUILD_DIR}/java
-javac -d ${BUILD_DIR}/java ${SOURCE_DIR}/src/java/*.java
-javah -o ${BUILD_DIR}/java/Tokenizer.h -cp ${BUILD_DIR}/java com.coccoc.Tokenizer
+javac -h ${BUILD_DIR}/java -d ${BUILD_DIR}/java ${SOURCE_DIR}/src/java/*.java
 
 g++ -shared -Wall -Werror -std=c++11 -Wno-deprecated -O3 -DNDEBUG -ggdb -fPIC \
 	-I ${SOURCE_DIR}/.. \
@@ -25,4 +24,3 @@ g++ -shared -Wall -Werror -std=c++11 -Wno-deprecated -O3 -DNDEBUG -ggdb -fPIC \
 	${SOURCE_DIR}/src/jni/Tokenizer.cpp
 
 jar -cf ${BUILD_DIR}/coccoc-tokenizer.jar -C ${BUILD_DIR}/java .
-
