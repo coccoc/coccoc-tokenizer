f0c4271b4b588d4e388ccfd32dee71fa365fca79
Duy Do <doquocduy@gmail.com>
2021-04-15 09:46:29 +0700

    Supports building the lib on Mac

--- java/build_java.sh
+++ java/build_java.sh
@@ -5,22 +5,31 @@ if [ -z "$1" ]; then
 	exit 1
 fi
 
+
 SOURCE_DIR=`dirname $0`
 BUILD_DIR="$1"
 
-JAVA_COMPILER=`readlink -f \`which javac\``
-JAVA_HOME=${JAVA_COMPILER%%/bin/javac}
+OS="$(uname | tr '[:upper:]' '[:lower:]')"
+if [ "${OS}" = "darwin" ]; then
+    # macOS
+    JAVA_HOME="$(/usr/libexec/java_home)"
+		OUTPUT_FILE='libcoccoc_tokenizer_jni.dylib'
+  else
+    JAVA_HOME="$(dirname $(dirname $(readlink -f $(which javac))))"
+		OUTPUT_FILE='libcoccoc_tokenizer_jni.so'
+  fi
+
 
 mkdir -p ${BUILD_DIR}/java
-javac -h ${BUILD_DIR}/java -d ${BUILD_DIR}/java ${SOURCE_DIR}/src/java/*.java
+${JAVA_HOME}/bin/javac -h ${BUILD_DIR}/java -d ${BUILD_DIR}/java ${SOURCE_DIR}/src/java/*.java
 
 g++ -shared -Wall -Werror -std=c++11 -Wno-deprecated -O3 -DNDEBUG -ggdb -fPIC \
 	-I ${SOURCE_DIR}/.. \
 	-I ${BUILD_DIR}/auto \
 	-I ${BUILD_DIR}/java \
 	-I ${JAVA_HOME}/include \
-	-I ${JAVA_HOME}/include/linux \
-	-o ${BUILD_DIR}/libcoccoc_tokenizer_jni.so \
+	-I ${JAVA_HOME}/include/${OS} \
+	-o ${BUILD_DIR}/${OUTPUT_FILE} \
 	${SOURCE_DIR}/src/jni/Tokenizer.cpp
 
 jar -cf ${BUILD_DIR}/coccoc-tokenizer.jar -C ${BUILD_DIR}/java .
